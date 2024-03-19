daba7ad5a9057df79ae407caed882235cb02091b
Paulo <paulosales@id.uff.br>
2024-03-19 15:31:40 +0000

    criado chamada de enderecos no método index do controller Home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     @store = Store.first
     @store_emails = StoreEmail.all
     @store_phones = StorePhone.all
-    #@store_adresses = StoreAdresses.all
+    @store_adresses = StoreAdresses.all
 
   end
 
